5b2177c7bc4a1e44e2b4e955d6d6c65f9940f3ef
saqlain <saqlainshahbaz59@gmail.com>
2024-06-05 15:30:01 +0500

    create react app and tailwind css

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-white'>
+      <h1>react app</h1>
+    </div>
+  );
+}
+
+export default App;
